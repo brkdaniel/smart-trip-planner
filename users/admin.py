20d718c42466090a16b4cc231111a4caf05d5868
brkdaniel <daniproburca@gmail.com>
2026-04-24 02:31:22 +0300

    setup django user + user_preference models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from users.models import UserPreference
 
-# Register your models here.
+
+admin.site.register(UserPreference)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserPreference(models.Model):
+    
+    PACE_CHOICES = [
+        ('slow', 'Slow'),
+        ('medium', 'Medium'),
+        ('fast', 'Fast'),
+    ]
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='preferences'
+    )
+    dietary_preference = models.CharField(
+        max_length=255,
+        blank=True
+    )
+    hotel_stars = models.IntegerField(
+        default=3
+    )
+    travel_pace = models.CharField(
+        max_length=10,
+        choices=PACE_CHOICES,
+        default='medium'
+    )
+    budget = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+    interests = models.TextField(
+        blank=True
+    )
+
+    class Meta:
+        db_table = 'user_preference'
+
+    def __str__(self):
+        return f"{self.user.username}'s preferences"
