e2fe28c3f273832ba9ecb0bab2fa99595c06c789
razvan-n25 <razvannica705@yahoo.com>
2026-04-29 00:57:38 +0300

    authentication+templates de test+cateva fixuri

--- users/views.py
+++ users/views.py
@@ -36,8 +36,7 @@ def signup_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-        
-            UserPreference.objects.create(user=user)
+            UserPreference.objects.get_or_create(user=user)
             login(request, user)
             return redirect('preferences')
     else:
@@ -47,20 +46,22 @@ def signup_view(request):
 
 
 def login_view(request):
-    next_url = request.POST.get('next') or request.GET.get('next')
+    next_url = request.POST.get('next') or request.GET.get('next') or ''
 
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
+            default_route = _default_authenticated_route(user)
+            if default_route == 'preferences':
+                return redirect('preferences')
             if next_url and url_has_allowed_host_and_scheme(
                 next_url,
                 allowed_hosts={request.get_host()},
                 require_https=request.is_secure(),
             ):
                 return redirect(next_url)
-            default_route = _default_authenticated_route(user)
             return redirect(default_route)
     else:
         form = AuthenticationForm(request)
