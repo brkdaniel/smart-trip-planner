e4376d3b468cb5e1051b0c4df5ee0c92e0f9f48b
Pavel Matei <pavelmatei2015@gmail.com>
2026-04-29 14:20:05 +0300

    am adaugat pagina de login+ scos logica de redirectionare spre preferences+ scos pagina de preferences

--- users/views.py
+++ users/views.py
@@ -1,30 +1,67 @@
 from django import forms
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.utils.http import url_has_allowed_host_and_scheme
 from django.views.decorators.http import require_http_methods
-from .models import UserPreference
 
 
-class UserPreferenceForm(forms.ModelForm):
-    class Meta:
-        model = UserPreference
-        fields = [
-            'budget',
-            'dietary_preference',
-            'travel_pace',
-            'interests',
-            'hotel_stars',
-        ]
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'autocomplete': 'off',
+    }))
+
+    class Meta(UserCreationForm.Meta):
+        fields = ('username', 'email')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'autocomplete': 'off',
+        })
+        self.fields['email'].widget.attrs.update({
+            'autocomplete': 'off',
+        })
+        self.fields['password1'].widget.attrs.update({
+            'autocomplete': 'new-password',
+        })
+        self.fields['password2'].widget.attrs.update({
+            'autocomplete': 'new-password',
+        })
 
 
-def _default_authenticated_route(user):
-    preference, _ = UserPreference.objects.get_or_create(user=user)
-    if preference.onboarding_completed:
-        return 'chat'
-    return 'preferences'
+class LoginForm(forms.Form):
+    email = forms.EmailField(label='Adresă email')
+    password = forms.CharField(
+        label='Parolă',
+        widget=forms.PasswordInput(attrs={
+            'autocomplete': 'off',
+        }),
+    )
+
+    def style_fields(self):
+        self.fields['email'].widget.attrs.update({
+            'class': 'login-input',
+            'placeholder': 'Adresă email',
+            'autocomplete': 'off',
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'login-input',
+            'placeholder': 'Parolă',
+            'autocomplete': 'off',
+        })
+        return self
+
+
+
 
 
 def home_view(request):
@@ -33,14 +70,13 @@ def home_view(request):
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
-            UserPreference.objects.get_or_create(user=user)
             login(request, user)
-            return redirect('preferences')
+            return redirect('chat')
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'html/signup.html', {'form': form})
 
 
@@ -48,23 +84,32 @@ def signup_view(request):
 def login_view(request):
     next_url = request.POST.get('next') or request.GET.get('next') or ''
 
+    def _authenticate_by_email(email, password):
+        user = User.objects.filter(email__iexact=email).first()
+        if user and user.check_password(password):
+            return user
+        return None
+
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = LoginForm(request.POST).style_fields()
         if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            default_route = _default_authenticated_route(user)
-            if default_route == 'preferences':
-                return redirect('preferences')
-            if next_url and url_has_allowed_host_and_scheme(
-                next_url,
-                allowed_hosts={request.get_host()},
-                require_https=request.is_secure(),
-            ):
-                return redirect(next_url)
-            return redirect(default_route)
+            user = _authenticate_by_email(
+                form.cleaned_data['email'],
+                form.cleaned_data['password'],
+            )
+            if user is None:
+                form.add_error(None, 'Email sau parolă invalide.')
+            else:
+                login(request, user)
+                if next_url and url_has_allowed_host_and_scheme(
+                    next_url,
+                    allowed_hosts={request.get_host()},
+                    require_https=request.is_secure(),
+                ):
+                    return redirect(next_url)
+                return redirect('chat')
     else:
-        form = AuthenticationForm(request)
+        form = LoginForm().style_fields()
     return render(request, 'html/login.html', {
         'form': form,
         'next': next_url,
@@ -76,26 +121,3 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
-
-
-
-@login_required
-def preferences_view(request):
-    preference, _ = UserPreference.objects.get_or_create(user=request.user)
-    if request.method == 'POST':
-        form = UserPreferenceForm(request.POST, instance=preference)
-        if form.is_valid():
-            was_completed = preference.onboarding_completed
-            updated_preference = form.save(commit=False)
-            updated_preference.onboarding_completed = True
-            updated_preference.save()
-            if was_completed:
-                return redirect('preferences')
-            return redirect('chat')
-    else:
-        form = UserPreferenceForm(instance=preference)
-
-    return render(request, 'html/preferences.html', {
-        'preference': preference,
-        'form': form,
-    })
